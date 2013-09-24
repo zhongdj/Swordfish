cdee2fff4da4425ef45c5cf853fbb449acafd04d
Barry Zhong <zhongdj@gmail.com>
2013-09-24 11:16:44 +0800

    add schedule group entity

---
+++ Business/Services/Scheduling/src/main/java/net/madz/scheduling/entities/ScheduleGroup.java
@@ -0,0 +1,10 @@
+package net.madz.scheduling.entities;
+
+import javax.persistence.Entity;
+
+import net.madz.authorization.entities.StandardObject;
+
+@Entity
+public class ScheduleGroup extends StandardObject {
+    
+}
