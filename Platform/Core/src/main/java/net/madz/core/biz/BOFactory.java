52ee20b3452ba486881f5fe519c1297278ec90a1
TracyLu <tastesnow@tracymatoMacBook-Pro.local>
2013-09-29 21:29:05 +0800

    Converted methods' parameter which is instanceof IBizObject to proxy when creating bussiness object.

--- Platform/Core/src/main/java/net/madz/core/biz/BOFactory.java
+++ Platform/Core/src/main/java/net/madz/core/biz/BOFactory.java
@@ -1,52 +1,83 @@
 package net.madz.core.biz;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.madz.core.entities.AbstractBaseEntity;
+import net.madz.lifecycle.impl.TransitionInvocationHandler;
+
 public class BOFactory {
 
-    public static <S extends T, T extends IBizObject<?>> S create(Class<T> proxyClass) {
+    public static <E extends AbstractBaseEntity, T extends IBizObject<E>> T create(Class<T> proxyClass) {
         BOProxy annotation = proxyClass.getAnnotation(BOProxy.class);
         if ( proxyClass.isAssignableFrom(annotation.value()) ) {
             @SuppressWarnings("unchecked")
-            Class<S> clz = (Class<S>) annotation.value();
+            Class<T> clz = (Class<T>) annotation.value();
             try {
-                return clz.newInstance();
+                return wrap(proxyClass, clz.newInstance());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         } else {
-            throw new RuntimeException("@BOProxy(value = " + annotation.value().getName()
-                    + "): value is not subclass of " + proxyClass.getName());
+            throw new RuntimeException("@BOProxy(value = " + annotation.value().getName() + "): value is not subclass of " + proxyClass.getName());
         }
     }
 
-    public static <E extends Object, S extends T, T extends IBizObject<E>> S create(Class<T> proxyClass, E entity) {
+    public static <E extends AbstractBaseEntity, T extends IBizObject<E>> T create(Class<T> proxyClass, E entity) {
         BOProxy annotation = proxyClass.getAnnotation(BOProxy.class);
         if ( proxyClass.isAssignableFrom(annotation.value()) ) {
             @SuppressWarnings("unchecked")
-            Class<S> clz = (Class<S>) annotation.value();
+            Class<T> clz = (Class<T>) annotation.value();
             try {
-                final Constructor<S> constructor = clz.getConstructor(entity.getClass());
-                return constructor.newInstance(entity);
+                final Constructor<T> constructor = clz.getConstructor(entity.getClass());
+                return wrap(proxyClass, constructor.newInstance(entity));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         } else {
-            throw new RuntimeException("@BOProxy(value = " + annotation.value().getName()
-                    + "): value is not subclass of " + proxyClass.getName());
+            throw new RuntimeException("@BOProxy(value = " + annotation.value().getName() + "): value is not subclass of " + proxyClass.getName());
         }
+    }
+
+    public static <E extends AbstractBaseEntity, T extends IBizObject<E>> List<T> create(Class<T> proxyClass, List<E> entities) {
+        final List<T> result = new ArrayList<>(entities.size());
+        for ( E entity : entities ) {
+            result.add((T) create(proxyClass, entity));
+        }
+        return result;
     }
 
     @SuppressWarnings("unchecked")
-    public static <E extends Object, S extends T, T extends IBizObject<E>> List<S> create(Class<T> proxyClass,
-            List<E> entities) {
-        final List<S> result = new ArrayList<>(entities.size());
-        for ( E entity : entities ) {
-            result.add((S) create(proxyClass, entity));
-        }
-        return result;
+    private static <E extends AbstractBaseEntity, T extends IBizObject<E>> T wrap(final Class<T> proxyClass, final T object) {
+        return (T) Proxy.newProxyInstance(BOFactory.class.getClassLoader(), new Class[] { proxyClass }, new InvocationHandler() {
+
+            @SuppressWarnings("rawtypes")
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                convertParameterToProxy(method, args);
+                //TransitionInvocationHandler tih = new TransitionInvocationHandler(object);
+                //Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] { proxyClass }, tih);
+                return method.invoke(object, args);
+            }
+
+            private void convertParameterToProxy(Method method, Object[] args) {
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                for ( int i = 0; i < parameterTypes.length; i++ ) {
+                    Class<?> c = parameterTypes[i];
+                    if ( c.isInterface() && IBizObject.class.isAssignableFrom(c) ) {
+                        Class<T> tClass = (Class<T>) c;
+                        IBizObject<E> bo = (IBizObject<E>) args[i];
+                        if ( AbstractBO.class.isAssignableFrom(args[i].getClass()) ) {
+                            args[i] = BOFactory.create(tClass, bo.get());
+                        }
+                    }
+                }
+            }
+        });
     }
 
     private BOFactory() {
