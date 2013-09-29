5ec78f6f8521b3cdc04458bfbd1cf9efdef9f830
TracyLu <tastesnow@tracymatoMacBook-Pro.local>
2013-09-29 22:13:45 +0800

    Added driverName and driverPhoneNumber

--- Business/Services/Scheduling/src/main/java/net/madz/scheduling/entities/ConcreteTruck.java
+++ Business/Services/Scheduling/src/main/java/net/madz/scheduling/entities/ConcreteTruck.java
@@ -10,27 +10,50 @@ import net.madz.authorization.entities.StandardObject;
 @Table(name = "concrete_truck")
 public class ConcreteTruck extends StandardObject {
 
-    private static final long serialVersionUID = -1132231092160514575L;
+	private static final long serialVersionUID = -1132231092160514575L;
 
-    @Column(name = "licence_plate_number", nullable = false)
-    private String licencePlateNumber;
+	@Column(name = "LICENCE_PLATE_NUMBER", nullable = false)
+	private String licencePlateNumber;
 
-    @Column(name = "rated_capacity", nullable = false)
-    private double ratedCapacity;
+	@Column(name = "RATED_CAPACITY", nullable = false)
+	private double ratedCapacity;
+	
+	@Column(name = "DRIVER_NAME", nullable = true)
+	private String driverName;
+	
+	@Column(name = "DRIVER_PHONE_NUMBER", nullable = true)
+	private String driverPhoneNumber;
 
-    public String getLicencePlateNumber() {
-        return licencePlateNumber;
-    }
+	public String getLicencePlateNumber() {
+		return licencePlateNumber;
+	}
 
-    public void setLicencePlateNumber(String licencePlateNumber) {
-        this.licencePlateNumber = licencePlateNumber;
-    }
+	public void setLicencePlateNumber(String licencePlateNumber) {
+		this.licencePlateNumber = licencePlateNumber;
+	}
 
-    public double getRatedCapacity() {
-        return ratedCapacity;
-    }
+	public double getRatedCapacity() {
+		return ratedCapacity;
+	}
 
-    public void setRatedCapacity(double ratedCapacity) {
-        this.ratedCapacity = ratedCapacity;
-    }
+	public void setRatedCapacity(double ratedCapacity) {
+		this.ratedCapacity = ratedCapacity;
+	}
+
+	public String getDriverName() {
+		return driverName;
+	}
+
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+
+	public String getDriverPhoneNumber() {
+		return driverPhoneNumber;
+	}
+
+	public void setDriverPhoneNumber(String driverPhoneNumber) {
+		this.driverPhoneNumber = driverPhoneNumber;
+	}
+	
 }
