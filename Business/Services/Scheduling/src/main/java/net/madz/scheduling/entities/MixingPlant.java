1f5f23a7d459b0a6c0e4719a541b5f680bbab823
TracyLu <tastesnow@tracymatoMacBook-Pro.local>
2013-09-29 21:31:39 +0800

    Added operator field for MixingPlant

--- Business/Services/Scheduling/src/main/java/net/madz/scheduling/entities/MixingPlant.java
+++ Business/Services/Scheduling/src/main/java/net/madz/scheduling/entities/MixingPlant.java
@@ -2,9 +2,13 @@ package net.madz.scheduling.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import net.madz.authorization.entities.StandardObject;
+import net.madz.authorization.entities.User;
 import net.madz.core.annotations.PinYinIndex;
 import net.madz.core.annotations.PinYinIndexed;
 
@@ -14,13 +18,15 @@ import net.madz.core.annotations.PinYinIndexed;
 public class MixingPlant extends StandardObject {
 
     private static final long serialVersionUID = 1113872983257589610L;
-
     @Column(nullable = false, length = 20)
     private String name;
-
     @Column(name = "PINYIN_ABBR_NAME", nullable = false, length = 20)
     @PinYinIndex(from = "name")
     private String pinyinAbbrName;
+    @OneToOne
+    @JoinColumns(value = { @JoinColumn(name = "TENANT_ID", nullable = false, insertable = false, updatable = false, referencedColumnName = "TENANT_ID"),
+            @JoinColumn(name = "OPERATOR_ID", nullable = false, insertable = true, updatable = false, referencedColumnName = "ID") })
+    private User operator;
 
     public String getName() {
         return name;
@@ -36,5 +42,13 @@ public class MixingPlant extends StandardObject {
 
     public void setPinyinAbbrName(String pinyinAbbrName) {
         this.pinyinAbbrName = pinyinAbbrName;
+    }
+
+    public User getOperator() {
+        return operator;
+    }
+
+    public void setOperator(User operator) {
+        this.operator = operator;
     }
 }
