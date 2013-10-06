b6c01f61b2ff0a4d38579e00576ef0c9074e4217
Barry Zhong <zhongdj@gmail.com>
2013-10-06 11:45:50 +0800

    add test metadata tasks to Lifecycle Module

---
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/meta/RelationalLifecyclePureMetadataTest.java
@@ -0,0 +1,206 @@
+package net.madz.lifecycle.meta;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RelationalLifecyclePureMetadataTest {
+
+    /* ///////////////////////////////////////////////////// */
+    /* /////////////////// Summary Part //////////////////// */
+    /* ///////////////////////////////////////////////////// */
+    @Test
+    public void testStateMachineMetadataSummary() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStateMachineInstSummary() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testTransitionMetadataSummary() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testTransitionInstSummary() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStateMetadataSummary() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStateInstSummary() {
+        fail("Not yet implemented");
+    }
+
+    /* ///////////////////////////////////////////////////// */
+    /* //////////////////// Serviceable //////////////////// */
+    /* ///////////////////////////////////////////////////// */
+    @Test
+    public void testServiceableStateMachineMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testServiceableTransitionMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testServiceableStateMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testServiceOrderStateMachineInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testServiceOrderTransitionInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testServiceOrderStateInst() {
+        fail("Not yet implemented");
+    }
+
+    /* ///////////////////////////////////////////////////// */
+    /* //////////////////// VehicleOrder /////////////////// */
+    /* ///////////////////////////////////////////////////// */
+    @Test
+    public void testVehicleScheduleOrderStateMachineMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleScheduleOrderTransitionMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleScheduleOrderStateMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleScheduleOrderStateMachineInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleScheduleOrderTransitionInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleScheduleOrderStateInst() {
+        fail("Not yet implemented");
+    }
+
+    /* ///////////////////////////////////////////////////// */
+    /* ////////////////////// PlantOrder /////////////////// */
+    /* ///////////////////////////////////////////////////// */
+    @Test
+    public void testPlantScheduleOrderStateMachineMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantScheduleOrderTransitionMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantScheduleOrderStateMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantScheduleOrderStateMachineInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantScheduleOrderTransitionInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantScheduleOrderStateInst() {
+        fail("Not yet implemented");
+    }
+
+    /* ///////////////////////////////////////////////////// */
+    /* //////////////////// VehicleResource //////////////// */
+    /* ///////////////////////////////////////////////////// */
+    @Test
+    public void testVehicleResourceStateMachineMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleResourceTransitionMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleResourceStateMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleResourceStateMachineInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleResourceTransitionInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVehicleResourceStateInst() {
+        fail("Not yet implemented");
+    }
+
+    /* ///////////////////////////////////////////////////// */
+    /* //////////////////// PlantResource ////////////////// */
+    /* ///////////////////////////////////////////////////// */
+    @Test
+    public void testPlantResourceStateMachineMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantResourceTransitionMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantResourceStateMetadata() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantResourceStateMachineInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantResourceTransitionInst() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPlantResourceStateInst() {
+        fail("Not yet implemented");
+    }
+}
