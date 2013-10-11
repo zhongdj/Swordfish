e2fc01937d8b27837e33437b2930a22ec1fb55ae
Barry Zhong <zhongdj@gmail.com>
2013-10-11 14:00:35 +0800

    [TA35] Define exception messages properties of scheduling module

--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/demo/inheritance/meta/ServiceOrderLifecycleMeta.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/demo/inheritance/meta/ServiceOrderLifecycleMeta.java
@@ -17,19 +17,17 @@ public interface ServiceOrderLifecycleMeta extends OrderLifecycleMeta {
     @StateSet
     public class States extends OrderLifecycleMeta.States {
 
-        @InboundWhiles({ @InboundWhile(
-                relation = SummaryPlan.class,
-                on = { SummaryPlanLifecycleMeta.States.Ongoing.class },
-                otherwise = {
-                        @ErrorMessage(states = { SummaryPlanLifecycleMeta.States.VolumeLeftEmpty.class }, bundle = "",
-                                code = ""),
-                        @ErrorMessage(states = { SummaryPlanLifecycleMeta.States.Done.class }, bundle = "", code = "") }) })
+        @InboundWhiles({ @InboundWhile(relation = SummaryPlan.class,
+                on = { SummaryPlanLifecycleMeta.States.Ongoing.class }, otherwise = {
+                        @ErrorMessage(states = { SummaryPlanLifecycleMeta.States.VolumeLeftEmpty.class },
+                                bundle = "scheduling", code = "100-0002"),
+                        @ErrorMessage(states = { SummaryPlanLifecycleMeta.States.Done.class }, bundle = "scheduling",
+                                code = "100-0003") }) })
         public static class Created extends OrderLifecycleMeta.States.Created {}
 
         @InboundWhiles({
                 @InboundWhile(relation = PlantResource.class, on = { PlantResourceLifecycleMeta.States.Idle.class,
-                        PlantResourceLifecycleMeta.States.Busy.class }, otherwise = { @ErrorMessage(states = {},
-                        code = "", bundle = "") }),
+                        PlantResourceLifecycleMeta.States.Busy.class }),
                 @InboundWhile(relation = ConcreteTruckResource.class, on = {
                         ConcreteTruckResourceLifecycleMeta.States.Idle.class,
                         ConcreteTruckResourceLifecycleMeta.States.Busy.class }) })
