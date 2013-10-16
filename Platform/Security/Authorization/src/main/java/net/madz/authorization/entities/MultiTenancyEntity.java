240f11dd9ed1fb82911c005cedb98a24a9607a9c
Barry Zhong <zhongdj@gmail.com>
2013-10-16 10:38:11 +0800

    Fix deploy error caused by @Access(AccessType.FIELD)
    eclipselink with cause error due to there is no field inside target
    class

--- Platform/Security/Authorization/src/main/java/net/madz/authorization/entities/MultiTenancyEntity.java
+++ Platform/Security/Authorization/src/main/java/net/madz/authorization/entities/MultiTenancyEntity.java
@@ -4,8 +4,6 @@
  */
 package net.madz.authorization.entities;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.MappedSuperclass;
 
@@ -18,7 +16,6 @@ import org.eclipse.persistence.annotations.TenantDiscriminatorColumn;
  * @author a
  */
 @MappedSuperclass
-@Access(AccessType.FIELD)
 @Multitenant(MultitenantType.SINGLE_TABLE)
 @TenantDiscriminatorColumn(name = "TENANT_ID", contextProperty = "tenant.id", discriminatorType = DiscriminatorType.INTEGER, primaryKey = true)
 public class MultiTenancyEntity extends StandardObject {
