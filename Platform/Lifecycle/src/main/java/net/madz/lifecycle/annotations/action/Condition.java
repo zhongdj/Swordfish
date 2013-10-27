8a4e7f17b3c22b4820b81dd2c3c3e66e8ab89476
Barry Zhong <zhongdj@gmail.com>
2013-10-27 09:31:49 +0800

    [Lifecycle] Add validation phase

--- Platform/Lifecycle/src/main/java/net/madz/lifecycle/annotations/action/Condition.java
+++ Platform/Lifecycle/src/main/java/net/madz/lifecycle/annotations/action/Condition.java
@@ -5,10 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Condition {
 
     Class<?> value();
+
+    boolean postValidate() default false;
 }
