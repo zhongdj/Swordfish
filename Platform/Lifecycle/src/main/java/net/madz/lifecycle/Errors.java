968a1ff962c5ac1858875a0adf5200cee96464a2
TracyLu <hailinglu@gmail.com>
2013-10-27 16:44:16 +0800

    [Lifecycle] Added error codes and error messages

--- Platform/Lifecycle/src/main/java/net/madz/lifecycle/Errors.java
+++ Platform/Lifecycle/src/main/java/net/madz/lifecycle/Errors.java
@@ -1,6 +1,7 @@
 package net.madz.lifecycle;
 
 public interface Errors {
+
     public static final String REGISTERED_META_ERROR = "002-1000";
     public static final String STATEMACHINE_SUPER_MUST_BE_STATEMACHINE = "002-2100";
     public static final String STATEMACHINE_WITHOUT_STATESET = "002-2201";
@@ -14,4 +15,16 @@ public interface Errors {
     public static final String INVALID_TRANSITION_REFERENCE = "002-2610";
     public static final String CONDITIONAL_TRANSITION_WITHOUT_CONDITION = "002-2611";
     public static final String TRANSITION_REFERENCE_BEYOND_COMPOSITE_STATE_SCOPE = "002-2612";
+    public static final String NEXT_STATESET_OF_FUNCTION_INVALID = "002-2700";
+    public static final String SHORT_CUT_INVALID = "002-2800";
+    public static final String RELATION_ATTRIBUTE_OF_INBOUNDWHILE_INVALID = "002-2911";
+    public static final String ON_ATTRIBUTE_OF_INBOUNDWHILE_INVALID = "002-2912";
+    public static final String OTHERWISE_ATTRIBUTE_OF_INBOUNDWHILE_INVALID = "002-2913";
+    public static final String RELATION_ATTRIBUTE_OF_VALIDWHILE_INVALID = "002-2921";
+    public static final String ON_ATTRIBUTE_OF_VALIDWHILE_INVALID = "002-2922";
+    public static final String LM_VALUE_MUST_BE_STATEMACHINE = "002-3100";
+    public static final String LM_MUST_CONCRETE_ALL_TRANSITIONS = "002-3201";
+    public static final String LM_MUST_CONCRETE_ALL_RELATIONS = "002-3202";
+    public static final String LM_MUST_CONCRETE_ALL_CONDITIONS = "002-3203";
+    public static final String LM_MUST_HAVE_STATEINDICATOR = "002-3300";
 }
