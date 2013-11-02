23a2812493b8456324c92c3c1eac7984bb1569b5
TracyLu <hailinglu@gmail.com>
2013-11-02 22:49:26 +0800

    Added unitTest for getAllStates method

---
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/AllTestSuites.java
@@ -0,0 +1,13 @@
+package net.madz.lifecycle;
+
+import net.madz.lifecycle.semantics.SemanticsTestSuite;
+import net.madz.lifecycle.syntax.SyntaxTestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({SyntaxTestSuite.class, SemanticsTestSuite.class})
+public class AllTestSuites {}
