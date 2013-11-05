7083235521d26e1d99d48cc5088a2961bccb82a3
TracyLu <hailinglu@gmail.com>
2013-11-05 21:23:40 +0800

    Added LM condition coverage positive and negative tests.

---
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/condition/LMSyntaxConditionNegativeTest.java
@@ -0,0 +1,66 @@
+package net.madz.lifecycle.syntax.lm.condition;
+
+import net.madz.lifecycle.AbsStateMachineRegistry;
+import net.madz.lifecycle.AbsStateMachineRegistry.LifecycleRegistry;
+import net.madz.lifecycle.AbsStateMachineRegistry.StateMachineBuilder;
+import net.madz.lifecycle.Errors;
+import net.madz.lifecycle.syntax.lm.condition.LMSyntaxConditionMetadata.S1.Conditions.S1_Condition_A;
+import net.madz.verification.VerificationException;
+
+import org.junit.Test;
+
+public class LMSyntaxConditionNegativeTest extends LMSyntaxConditionMetadata {
+
+    @Test(expected = VerificationException.class)
+    public final void test_condition_reference_invalid() throws VerificationException, NoSuchMethodException, SecurityException {
+        @LifecycleRegistry(NLM_1.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(), Errors.LM_CONDITION_REFERENCE_INVALID,
+                    NLM_1.class.getMethod("getConditionA", null),
+                    S1.Transitions.S1_Transition_X.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public final void test_multiple_methods_reference_same_condition() throws VerificationException {
+        @LifecycleRegistry(NLM_2.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    Errors.LM_CONDITION_MULTIPLE_METHODS_REFERENCE_SAME_CONDITION, NLM_2.class,
+                    S1.Conditions.S1_Condition_A.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public final void test_condition_not_covered() throws VerificationException {
+        @LifecycleRegistry(NLM_3.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(), Errors.LM_CONDITION_NOT_COVERED,
+                    NLM_3.class, S1.class.getName(), S1.class.getName() + ".ConditionSet." + S1_Condition_A.class.getSimpleName());
+            throw e;
+        }
+    }
+}
