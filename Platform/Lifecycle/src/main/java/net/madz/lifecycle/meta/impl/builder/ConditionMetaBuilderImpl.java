a0bd27075a7937081578bcba91008d3abd97ae33
TracyLu <hailinglu@gmail.com>
2013-11-05 21:43:18 +0800

    Refactored ConditionMetadata interface to support matching by key

--- Platform/Lifecycle/src/main/java/net/madz/lifecycle/meta/impl/builder/ConditionMetaBuilderImpl.java
+++ Platform/Lifecycle/src/main/java/net/madz/lifecycle/meta/impl/builder/ConditionMetaBuilderImpl.java
@@ -2,13 +2,16 @@ package net.madz.lifecycle.meta.impl.builder;
 
 import net.madz.lifecycle.meta.builder.ConditionMetaBuilder;
 import net.madz.lifecycle.meta.builder.StateMachineMetaBuilder;
+import net.madz.meta.MetaData;
+import net.madz.meta.MetaDataFilter;
+import net.madz.meta.MetaDataFilterable;
 import net.madz.verification.VerificationFailureSet;
 
 public class ConditionMetaBuilderImpl extends AnnotationMetaBuilderBase<ConditionMetaBuilder, StateMachineMetaBuilder>
         implements ConditionMetaBuilder {
 
     protected ConditionMetaBuilderImpl(StateMachineMetaBuilder parent, String name) {
-        super(parent, name);
+        super(parent, "ConditionSet." + name);
     }
 
     @Override
@@ -18,5 +21,11 @@ public class ConditionMetaBuilderImpl extends AnnotationMetaBuilderBase<Conditio
     public ConditionMetaBuilder build(Class<?> klass, StateMachineMetaBuilder builder) {
         addKeys(klass);
         return this;
+    }
+
+    @Override
+    public MetaDataFilterable filter(MetaData parent, MetaDataFilter filter, boolean lazyFilter) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
--- Platform/Lifecycle/src/main/java/net/madz/lifecycle/meta/template/ConditionMetadata.java
+++ Platform/Lifecycle/src/main/java/net/madz/lifecycle/meta/template/ConditionMetadata.java
@@ -1,6 +1,6 @@
 package net.madz.lifecycle.meta.template;
 
 import net.madz.meta.MetaData;
+import net.madz.meta.MetaDataFilterable;
 
-
-public interface ConditionMetadata extends MetaData {}
+public interface ConditionMetadata extends MetaData, MetaDataFilterable {}
