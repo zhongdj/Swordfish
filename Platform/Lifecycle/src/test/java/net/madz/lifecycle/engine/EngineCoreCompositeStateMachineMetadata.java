2590df3b55b4b8484d7bae182feeccd5fea2fa3c
Barry Zhong <zhongdj@gmail.com>
2013-11-11 19:38:07 +0800

    Add 3 relational positive test scenario for composite state machine

--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/engine/EngineCoreCompositeStateMachineMetadata.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/engine/EngineCoreCompositeStateMachineMetadata.java
@@ -2,12 +2,16 @@ package net.madz.lifecycle.engine;
 
 import net.madz.lifecycle.annotations.CompositeStateMachine;
 import net.madz.lifecycle.annotations.Function;
+import net.madz.lifecycle.annotations.Functions;
 import net.madz.lifecycle.annotations.LifecycleMeta;
-import net.madz.lifecycle.annotations.StateIndicator;
 import net.madz.lifecycle.annotations.StateMachine;
 import net.madz.lifecycle.annotations.StateSet;
 import net.madz.lifecycle.annotations.Transition;
 import net.madz.lifecycle.annotations.TransitionSet;
+import net.madz.lifecycle.annotations.relation.RelateTo;
+import net.madz.lifecycle.annotations.relation.Relation;
+import net.madz.lifecycle.annotations.relation.RelationSet;
+import net.madz.lifecycle.annotations.relation.ValidWhile;
 import net.madz.lifecycle.annotations.state.End;
 import net.madz.lifecycle.annotations.state.Initial;
 import net.madz.lifecycle.annotations.state.ShortCut;
@@ -22,8 +26,11 @@ public class EngineCoreCompositeStateMachineMetadata extends EngineTestBase {
         registerMetaFromClass(EngineCoreCompositeStateMachineMetadata.class);
     }
 
+    // ///////////////////////////////////////////////////////////////////////////////
+    // Non Relational
+    // ///////////////////////////////////////////////////////////////////////////////
     @StateMachine
-    static interface BaseLifecycle {
+    static interface OrderLifecycle {
 
         @StateSet
         static interface States {
@@ -39,17 +46,17 @@ public class EngineCoreCompositeStateMachineMetadata extends EngineTestBase {
                 static interface SubStates {
 
                     @Initial
-                    @Function(transition = BaseLifecycle.States.Started.SubTransitions.DoProduce.class,
+                    @Function(transition = OrderLifecycle.States.Started.SubTransitions.DoProduce.class,
                             value = Producing.class)
                     static interface OrderCreated {}
-                    @Function(transition = BaseLifecycle.States.Started.SubTransitions.DoDeliver.class,
+                    @Function(transition = OrderLifecycle.States.Started.SubTransitions.DoDeliver.class,
                             value = Delivering.class)
                     static interface Producing {}
-                    @Function(transition = BaseLifecycle.States.Started.SubTransitions.ConfirmComplete.class,
+                    @Function(transition = OrderLifecycle.States.Started.SubTransitions.ConfirmComplete.class,
                             value = Done.class)
                     static interface Delivering {}
                     @End
-                    @ShortCut(BaseLifecycle.States.Finished.class)
+                    @ShortCut(OrderLifecycle.States.Finished.class)
                     static interface Done {}
                 }
                 @TransitionSet
@@ -72,12 +79,7 @@ public class EngineCoreCompositeStateMachineMetadata extends EngineTestBase {
             static interface Cancel {}
         }
     }
-    @LifecycleMeta(BaseLifecycle.class)
-    public static class Product extends ReactiveObject {
-
-        public Product() {
-            initialState(BaseLifecycle.States.Created.class.getSimpleName());
-        }
+    public abstract static class ProductBase extends ReactiveObject {
 
         @Transition
         public void start() {}
@@ -93,5 +95,300 @@ public class EngineCoreCompositeStateMachineMetadata extends EngineTestBase {
 
         @Transition
         public void confirmComplete() {}
+    }
+    @LifecycleMeta(OrderLifecycle.class)
+    public static class ProductOrder extends ProductBase {
+
+        public ProductOrder() {
+            initialState(OrderLifecycle.States.Created.class.getSimpleName());
+        }
+    }
+    // ///////////////////////////////////////////////////////////////////////////////
+    // Relational
+    // ///////////////////////////////////////////////////////////////////////////////
+    @StateMachine
+    static interface ContractLifecycle {
+
+        @StateSet
+        static interface States {
+
+            @Initial
+            @Functions({ @Function(transition = ContractLifecycle.Transitions.Activate.class, value = Active.class),
+                    @Function(transition = ContractLifecycle.Transitions.Cancel.class, value = Canceled.class) })
+            static interface Draft {}
+            @Functions({ @Function(transition = ContractLifecycle.Transitions.Expire.class, value = Expired.class),
+                    @Function(transition = ContractLifecycle.Transitions.Cancel.class, value = Canceled.class) })
+            static interface Active {}
+            @End
+            static interface Expired {}
+            @End
+            static interface Canceled {}
+        }
+        @TransitionSet
+        static interface Transitions {
+
+            static interface Activate {}
+            static interface Expire {}
+            static interface Cancel {}
+        }
+    }
+    @LifecycleMeta(ContractLifecycle.class)
+    public static class Contract extends ReactiveObject {
+
+        public Contract() {
+            initialState(ContractLifecycle.States.Draft.class.getSimpleName());
+        }
+
+        @Transition
+        public void activate() {}
+
+        @Transition
+        public void expire() {}
+
+        @Transition
+        public void cancel() {}
+    }
+    // ///////////////////////////////////////////////////////////////////////////////
+    // Relational Case 1.
+    // ///////////////////////////////////////////////////////////////////////////////
+    @StateMachine
+    static interface RelationalOrderLifecycleSharingValidWhile {
+
+        @StateSet
+        static interface States {
+
+            @Initial
+            @Function(transition = Transitions.Start.class, value = Started.class)
+            @ValidWhile(on = { ContractLifecycle.States.Active.class }, relation = Relations.Contract.class)
+            static interface Created {}
+            @CompositeStateMachine
+            @Function(transition = Transitions.Cancel.class, value = Canceled.class)
+            static interface Started {
+
+                @StateSet
+                static interface SubStates {
+
+                    @Initial
+                    @Function(
+                            transition = RelationalOrderLifecycleSharingValidWhile.States.Started.SubTransitions.DoProduce.class,
+                            value = Producing.class)
+                    @ValidWhile(on = { ContractLifecycle.States.Active.class },
+                            relation = RelationalOrderLifecycleSharingValidWhile.Relations.Contract.class)
+                    static interface OrderCreated {}
+                    @Function(
+                            transition = RelationalOrderLifecycleSharingValidWhile.States.Started.SubTransitions.DoDeliver.class,
+                            value = Delivering.class)
+                    @ValidWhile(on = { ContractLifecycle.States.Active.class },
+                            relation = RelationalOrderLifecycleSharingValidWhile.Relations.Contract.class)
+                    static interface Producing {}
+                    @Function(
+                            transition = RelationalOrderLifecycleSharingValidWhile.States.Started.SubTransitions.ConfirmComplete.class,
+                            value = Done.class)
+                    @ValidWhile(on = { ContractLifecycle.States.Active.class },
+                            relation = RelationalOrderLifecycleSharingValidWhile.Relations.Contract.class)
+                    static interface Delivering {}
+                    @End
+                    @ShortCut(RelationalOrderLifecycleSharingValidWhile.States.Finished.class)
+                    // Ignoring : @ValidWhile(on = {
+                    // ContractLifecycle.States.Active.class }, relation =
+                    // Relations.Contract.class)
+                    static interface Done {}
+                }
+                @TransitionSet
+                static interface SubTransitions {
+
+                    static interface DoProduce {}
+                    static interface DoDeliver {}
+                    static interface ConfirmComplete {}
+                }
+            }
+            @End
+            static interface Finished {}
+            @End
+            static interface Canceled {}
+        }
+        @TransitionSet
+        static interface Transitions {
+
+            static interface Start {}
+            static interface Cancel {}
+        }
+        @RelationSet
+        static interface Relations {
+
+            @RelateTo(ContractLifecycle.class)
+            static interface Contract {}
+        }
+    }
+    @LifecycleMeta(RelationalOrderLifecycleSharingValidWhile.class)
+    public static class ProductOrderSharingValidWhile extends ProductBase {
+
+        private Contract contract;
+
+        public ProductOrderSharingValidWhile(Contract contract) {
+            this.contract = contract;
+            initialState(RelationalOrderLifecycleSharingValidWhile.States.Created.class.getSimpleName());
+        }
+
+        @Relation(RelationalOrderLifecycleSharingValidWhile.Relations.Contract.class)
+        public Contract getContract() {
+            return this.contract;
+        }
+    }
+    // ///////////////////////////////////////////////////////////////////////////////
+    // Relational Case 2.
+    // ///////////////////////////////////////////////////////////////////////////////
+    @StateMachine
+    static interface RelationalOrderLifecycleReferencingOuterValidWhile {
+
+        @StateSet
+        static interface States {
+
+            @Initial
+            @Function(transition = Transitions.Start.class, value = Started.class)
+            @ValidWhile(on = { ContractLifecycle.States.Active.class }, relation = Relations.Contract.class)
+            static interface Created {}
+            @CompositeStateMachine
+            @Function(transition = Transitions.Cancel.class, value = Canceled.class)
+            @ValidWhile(on = { ContractLifecycle.States.Active.class }, relation = Relations.Contract.class)
+            static interface Started {
+
+                @StateSet
+                static interface SubStates {
+
+                    @Initial
+                    @Function(
+                            transition = RelationalOrderLifecycleReferencingOuterValidWhile.States.Started.SubTransitions.DoProduce.class,
+                            value = Producing.class)
+                    static interface OrderCreated {}
+                    @Function(
+                            transition = RelationalOrderLifecycleReferencingOuterValidWhile.States.Started.SubTransitions.DoDeliver.class,
+                            value = Delivering.class)
+                    static interface Producing {}
+                    @Function(
+                            transition = RelationalOrderLifecycleReferencingOuterValidWhile.States.Started.SubTransitions.ConfirmComplete.class,
+                            value = Done.class)
+                    static interface Delivering {}
+                    @End
+                    @ShortCut(RelationalOrderLifecycleReferencingOuterValidWhile.States.Finished.class)
+                    static interface Done {}
+                }
+                @TransitionSet
+                static interface SubTransitions {
+
+                    static interface DoProduce {}
+                    static interface DoDeliver {}
+                    static interface ConfirmComplete {}
+                }
+            }
+            @End
+            static interface Finished {}
+            @End
+            static interface Canceled {}
+        }
+        @TransitionSet
+        static interface Transitions {
+
+            static interface Start {}
+            static interface Cancel {}
+        }
+        @RelationSet
+        static interface Relations {
+
+            @RelateTo(ContractLifecycle.class)
+            static interface Contract {}
+        }
+    }
+    @LifecycleMeta(RelationalOrderLifecycleReferencingOuterValidWhile.class)
+    public static class ProductOrderOuterValidWhile extends ProductBase {
+
+        private final Contract contract;
+
+        public ProductOrderOuterValidWhile(Contract contract) {
+            initialState(RelationalOrderLifecycleReferencingOuterValidWhile.States.Created.class.getSimpleName());
+            this.contract = contract;
+        }
+
+        @Relation(RelationalOrderLifecycleReferencingOuterValidWhile.Relations.Contract.class)
+        public Contract getContract() {
+            return this.contract;
+        }
+    }
+    @StateMachine
+    static interface RelationalOrderLifecycleReferencingInnerValidWhile {
+
+        @StateSet
+        static interface States {
+
+            @Initial
+            @Function(transition = Transitions.Start.class, value = Started.class)
+            static interface Created {}
+            @CompositeStateMachine
+            @Function(transition = Transitions.Cancel.class, value = Canceled.class)
+            static interface Started {
+
+                @StateSet
+                static interface SubStates {
+
+                    @Initial
+                    @Function(
+                            transition = RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.SubTransitions.DoProduce.class,
+                            value = Producing.class)
+                    static interface OrderCreated {}
+                    @Function(
+                            transition = RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.SubTransitions.DoDeliver.class,
+                            value = Delivering.class)
+                    static interface Producing {}
+                    @Function(
+                            transition = RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.SubTransitions.ConfirmComplete.class,
+                            value = Done.class)
+                    static interface Delivering {}
+                    @End
+                    @ShortCut(RelationalOrderLifecycleReferencingInnerValidWhile.States.Finished.class)
+                    static interface Done {}
+                }
+                @TransitionSet
+                static interface SubTransitions {
+
+                    static interface DoProduce {}
+                    static interface DoDeliver {}
+                    static interface ConfirmComplete {}
+                }
+                @RelationSet
+                static interface Relations {
+
+                    @RelateTo(ContractLifecycle.class)
+                    static interface Contract {}
+                }
+            }
+            @End
+            static interface Finished {}
+            @End
+            static interface Canceled {}
+        }
+        @TransitionSet
+        static interface Transitions {
+
+            static interface Start {}
+            static interface Cancel {}
+        }
+    }
+    // ///////////////////////////////////////////////////////////////////////////////
+    // Relational Case 3.
+    // ///////////////////////////////////////////////////////////////////////////////
+    @LifecycleMeta(RelationalOrderLifecycleReferencingInnerValidWhile.class)
+    public static class ProductOrderInnerValidWhile extends ProductBase {
+
+        private final Contract contract;
+
+        public ProductOrderInnerValidWhile(Contract contract) {
+            initialState(RelationalOrderLifecycleReferencingInnerValidWhile.States.Created.class.getSimpleName());
+            this.contract = contract;
+        }
+
+        @Relation(RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.Relations.Contract.class)
+        public Contract getContract() {
+            return contract;
+        }
     }
 }
--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/engine/EngineCoreCompositeStateMachinePositiveTests.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/engine/EngineCoreCompositeStateMachinePositiveTests.java
@@ -5,5 +5,153 @@ import org.junit.Test;
 public class EngineCoreCompositeStateMachinePositiveTests extends EngineCoreCompositeStateMachineMetadata {
 
     @Test
-    public void test_non_relational_composite_state_machine() {}
+    public void test_non_relational_composite_state_machine_complete_process() {
+        final ProductOrder product = new ProductOrder();
+        // Outer State + Outer Transition => Composite State => Composite
+        // Initial State
+        {
+            // Outer Initial State
+            assertState(OrderLifecycle.States.Created.class, product);
+            // Outer Transition
+            product.start();
+            assertState(OrderLifecycle.States.Started.SubStates.OrderCreated.class, product);
+        }
+        {
+            // Composite State + Composite Transition => Composite State
+            product.doProduce();
+            assertState(OrderLifecycle.States.Started.SubStates.Producing.class, product);
+            product.doDeliver();
+            assertState(OrderLifecycle.States.Started.SubStates.Delivering.class, product);
+        }
+        {
+            // Composite State + Composite Transition => Composite Final State
+            // => Outer State
+            product.confirmComplete();
+            assertState(OrderLifecycle.States.Finished.class, product);
+        }
+    }
+
+    @Test
+    public void test_non_relational_composite_state_machine_cancel_process_with_outer_transition() {
+        final ProductOrder product = new ProductOrder();
+        {// Outer State + Outer Transition => Composite State => Composite
+         // Initial State
+            assertState(OrderLifecycle.States.Created.class, product);
+            product.start();
+            assertState(OrderLifecycle.States.Started.SubStates.OrderCreated.class, product);
+        }
+        {
+            // Composite State + Composite Transition => Composite State
+            product.doProduce();
+            assertState(OrderLifecycle.States.Started.SubStates.Producing.class, product);
+        }
+        {
+            // Composite State + Composite Transition => Composite Final State
+            // => Outer State
+            product.cancel();
+            assertState(OrderLifecycle.States.Canceled.class, product);
+        }
+    }
+
+    @Test
+    public void test_relational_composite_state_machine_sharing_with_composite_state() {
+        final Contract contract = new Contract();
+        // Outer Initial State
+        assertState(ContractLifecycle.States.Draft.class, contract);
+        contract.activate();
+        assertState(ContractLifecycle.States.Active.class, contract);
+        final ProductOrderSharingValidWhile product = new ProductOrderSharingValidWhile(contract);
+        // Outer State + Outer Transition => Composite State => Composite
+        // Initial State
+        {
+            // Outer Initial State
+            assertState(RelationalOrderLifecycleSharingValidWhile.States.Created.class, product);
+            // Outer Transition
+            product.start();
+            assertState(RelationalOrderLifecycleSharingValidWhile.States.Started.SubStates.OrderCreated.class, product);
+        }
+        {
+            // Composite State + Composite Transition => Composite State
+            product.doProduce();
+            assertState(RelationalOrderLifecycleSharingValidWhile.States.Started.SubStates.Producing.class, product);
+            product.doDeliver();
+            assertState(RelationalOrderLifecycleSharingValidWhile.States.Started.SubStates.Delivering.class, product);
+        }
+        {
+            // Composite State + Composite Transition => Composite Final State
+            // => Outer State
+            product.confirmComplete();
+            assertState(RelationalOrderLifecycleSharingValidWhile.States.Finished.class, product);
+        }
+    }
+
+    @Test
+    public void test_relational_composite_state_machine_with_outer_relations() {
+        final Contract contract = new Contract();
+        // Outer Initial State
+        assertState(ContractLifecycle.States.Draft.class, contract);
+        contract.activate();
+        assertState(ContractLifecycle.States.Active.class, contract);
+        final ProductOrderOuterValidWhile product = new ProductOrderOuterValidWhile(contract);
+        // Outer State + Outer Transition => Composite State => Composite
+        // Initial State
+        {
+            // Outer Initial State
+            assertState(RelationalOrderLifecycleReferencingOuterValidWhile.States.Created.class, product);
+            // Outer Transition
+            product.start();
+            assertState(RelationalOrderLifecycleReferencingOuterValidWhile.States.Started.SubStates.OrderCreated.class,
+                    product);
+        }
+        {
+            // Composite State + Composite Transition => Composite State
+            product.doProduce();
+            assertState(RelationalOrderLifecycleReferencingOuterValidWhile.States.Started.SubStates.Producing.class,
+                    product);
+            product.doDeliver();
+            assertState(RelationalOrderLifecycleReferencingOuterValidWhile.States.Started.SubStates.Delivering.class,
+                    product);
+        }
+        {
+            // Composite State + Composite Transition => Composite Final State
+            // => Outer State
+            product.confirmComplete();
+            assertState(RelationalOrderLifecycleReferencingOuterValidWhile.States.Finished.class, product);
+        }
+    }
+
+    @Test
+    public void test_relational_composite_state_machine_with_inner_relations() {
+        final Contract contract = new Contract();
+        // Outer Initial State
+        assertState(ContractLifecycle.States.Draft.class, contract);
+        contract.activate();
+        assertState(ContractLifecycle.States.Active.class, contract);
+        final ProductOrderInnerValidWhile product = new ProductOrderInnerValidWhile(contract);
+        // Outer State + Outer Transition => Composite State => Composite
+        // Initial State
+        {
+            // Outer Initial State
+            assertState(RelationalOrderLifecycleReferencingInnerValidWhile.States.Created.class, product);
+            // Outer Transition
+            product.start();
+            assertState(RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.SubStates.OrderCreated.class,
+                    product);
+        }
+        {
+            // Composite State + Composite Transition => Composite State
+            product.doProduce();
+            assertState(RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.SubStates.Producing.class,
+                    product);
+            product.doDeliver();
+            assertState(RelationalOrderLifecycleReferencingInnerValidWhile.States.Started.SubStates.Delivering.class,
+                    product);
+        }
+        {
+            // Composite State + Composite Transition => Composite Final State
+            // => Outer State
+            product.confirmComplete();
+            assertState(RelationalOrderLifecycleReferencingInnerValidWhile.States.Finished.class, product);
+        }
+    }
 }
