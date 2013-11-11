ac6254e00bf2daf59ac57f9bddf1a27c4e80003f
TracyLu <hailinglu@gmail.com>
2013-11-11 22:30:20 +0800

    Fix Java Warnings

--- Platform/Lifecycle/src/main/java/net/madz/lifecycle/meta/instance/ErrorMessageObject.java
+++ Platform/Lifecycle/src/main/java/net/madz/lifecycle/meta/instance/ErrorMessageObject.java
@@ -5,11 +5,11 @@ import net.madz.lifecycle.meta.template.StateMetadata;
 public class ErrorMessageObject {
 
     private String bundle;
-    private Class classLoaderClass;
+    private Class<?> classLoaderClass;
     private String errorCode;
     private StateMetadata[] errorStates;
 
-    public ErrorMessageObject(String bundle, Class classLoaderClass, String errorCode, StateMetadata[] errorStates) {
+    public ErrorMessageObject(String bundle, Class<?> classLoaderClass, String errorCode, StateMetadata[] errorStates) {
         super();
         this.bundle = bundle;
         this.classLoaderClass = classLoaderClass;
@@ -17,25 +17,19 @@ public class ErrorMessageObject {
         this.errorStates = errorStates;
     }
 
-    
     public String getBundle() {
         return bundle;
     }
 
-    
-    public Class getClassLoaderClass() {
+    public Class<?> getClassLoaderClass() {
         return classLoaderClass;
     }
 
-    
     public String getErrorCode() {
         return errorCode;
     }
 
-    
     public StateMetadata[] getErrorStates() {
         return errorStates;
     }
-    
-    
 }
