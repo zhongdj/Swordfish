05a728efa465ec7f49acafc894c33d2b21d75be9
TracyLu <hailinglu@gmail.com>
2013-11-18 21:53:50 +0800

    Added some more callback negative tests

--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/callback/CallbackNegativeTests.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/callback/CallbackNegativeTests.java
@@ -5,6 +5,7 @@ import net.madz.lifecycle.AbsStateMachineRegistry;
 import net.madz.lifecycle.AbsStateMachineRegistry.LifecycleRegistry;
 import net.madz.lifecycle.AbsStateMachineRegistry.StateMachineBuilder;
 import net.madz.lifecycle.SyntaxErrors;
+import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S2;
 import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S1.States.S1_State_C;
 import net.madz.verification.VerificationException;
 
@@ -12,9 +13,9 @@ import org.junit.Test;
 
 public class CallbackNegativeTests extends CallbackTestBase {
 
-    
     @Test(expected = VerificationException.class)
-    public final void test_PreStateChange_To_Possible_Next_State() throws NoSuchMethodException, SecurityException, VerificationException {
+    public final void test_PreStateChange_To_Possible_Next_State() throws NoSuchMethodException, SecurityException,
+            VerificationException {
         @LifecycleRegistry(NLM_With_PreStateChange_To_Possible_Next_State.class)
         @StateMachineBuilder
         class Registry extends AbsStateMachineRegistry {
@@ -22,13 +23,198 @@ public class CallbackNegativeTests extends CallbackTestBase {
             protected Registry() throws VerificationException {
                 super();
             }
-            
         }
-        
         try {
             new Registry();
         } catch (VerificationException e) {
-            assertFailure(e.getVerificationFailureSet().iterator().next(), SyntaxErrors.PRE_STATE_CHANGE_TO_POST_EVALUATE_STATE_IS_INVALID, S1_State_C.class, NLM_With_PreStateChange_To_Possible_Next_State.class.getMethod("interceptStateChange", LifecycleInterceptor.class));
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.PRE_STATE_CHANGE_TO_POST_EVALUATE_STATE_IS_INVALID, S1_State_C.class,
+                    NLM_With_PreStateChange_To_Possible_Next_State.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class));
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_prestatechange_from_state_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_prestatechange_from_state_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.PRE_STATE_CHANGE_FROM_STATE_IS_INVALID, S2.States.S2_State_A.class,
+                    NLM_prestatechange_from_state_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), S1.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_prestatechange_to_state_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_prestatechange_to_state_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.PRE_STATE_CHANGE_TO_STATE_IS_INVALID, S2.States.S2_State_A.class,
+                    NLM_prestatechange_to_state_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), S1.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_poststatechange_from_state_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_poststatechange_from_state_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.POST_STATE_CHANGE_FROM_STATE_IS_INVALID, S2.States.S2_State_A.class,
+                    NLM_poststatechange_from_state_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), S1.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_poststatechange_to_state_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_poststatechange_to_state_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.POST_STATE_CHANGE_TO_STATE_IS_INVALID, S2.States.S2_State_A.class,
+                    NLM_poststatechange_to_state_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), S1.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_prestatechange_relation_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_prestatechange_relation_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.PRE_STATE_CHANGE_RELATION_INVALID, "s1",
+                    NLM_prestatechange_relation_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), NLM_prestatechange_relation_invalid.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_prestatechange_mappedby_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_prestatechange_mappedby_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.PRE_STATE_CHANGE_MAPPEDBY_INVALID, "s1",
+                    NLM_prestatechange_mappedby_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), S1BaseLM.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_poststatechange_relation_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_poststatechange_relation_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.POST_STATE_CHANGE_RELATION_INVALID, "s1",
+                    NLM_poststatechange_relation_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), NLM_poststatechange_relation_invalid.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_poststatechange_mappedby_invalid() throws VerificationException, NoSuchMethodException,
+            SecurityException {
+        @LifecycleRegistry(NLM_poststatechange_mappedby_invalid.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.POST_STATE_CHANGE_MAPPEDBY_INVALID, "s1",
+                    NLM_poststatechange_mappedby_invalid.class.getMethod("interceptStateChange",
+                            LifecycleInterceptor.class), S1BaseLM.class);
+            throw e;
+        }
+    }
+
+    @Test(expected = VerificationException.class)
+    public void test_callbacks_with_invalid_states() throws NoSuchMethodException, SecurityException,
+            VerificationException {
+        @LifecycleRegistry(NLM_With_CallBacksWithInvalidStates.class)
+        @StateMachineBuilder
+        class Registry extends AbsStateMachineRegistry {
+
+            protected Registry() throws VerificationException {}
+        }
+        try {
+            new Registry();
+        } catch (VerificationException e) {
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.PRE_STATE_CHANGE_FROM_STATE_IS_INVALID, S2.States.S2_State_A.class,
+                    NLM_With_CallBacksWithInvalidStates.class.getMethod("interceptStates", LifecycleInterceptor.class),
+                    S1.class);
+            assertFailure(e.getVerificationFailureSet().iterator().next(),
+                    SyntaxErrors.POST_STATE_CHANGE_TO_STATE_IS_INVALID, S2.States.S2_State_A.class,
+                    NLM_With_CallBacksWithInvalidStates.class.getMethod("interceptStates", LifecycleInterceptor.class),
+                    S1.class);
             throw e;
         }
     }
--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/callback/CallbackTestBase.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/callback/CallbackTestBase.java
@@ -3,6 +3,7 @@ package net.madz.lifecycle.syntax.lm.callback;
 import net.madz.bcel.intercept.LifecycleInterceptor;
 import net.madz.lifecycle.annotations.Function;
 import net.madz.lifecycle.annotations.LifecycleMeta;
+import net.madz.lifecycle.annotations.StateIndicator;
 import net.madz.lifecycle.annotations.StateMachine;
 import net.madz.lifecycle.annotations.StateSet;
 import net.madz.lifecycle.annotations.Transition;
@@ -11,13 +12,21 @@ import net.madz.lifecycle.annotations.action.Condition;
 import net.madz.lifecycle.annotations.action.ConditionSet;
 import net.madz.lifecycle.annotations.action.Conditional;
 import net.madz.lifecycle.annotations.action.ConditionalTransition;
+import net.madz.lifecycle.annotations.callback.Callbacks;
+import net.madz.lifecycle.annotations.callback.PostStateChange;
 import net.madz.lifecycle.annotations.callback.PreStateChange;
+import net.madz.lifecycle.annotations.relation.InboundWhile;
+import net.madz.lifecycle.annotations.relation.RelateTo;
+import net.madz.lifecycle.annotations.relation.Relation;
+import net.madz.lifecycle.annotations.relation.RelationSet;
 import net.madz.lifecycle.annotations.state.End;
 import net.madz.lifecycle.annotations.state.Initial;
 import net.madz.lifecycle.syntax.BaseMetaDataTest;
 import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S1.Conditions.S1_Condition_A;
 import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S1.States.S1_State_B;
 import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S1.States.S1_State_C;
+import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S2.Relations.S1Relation;
+import net.madz.lifecycle.syntax.lm.callback.CallbackTestBase.S2.Transitions.Move;
 
 public abstract class CallbackTestBase extends BaseMetaDataTest {
 
@@ -61,23 +70,34 @@ public abstract class CallbackTestBase extends BaseMetaDataTest {
             }
         }
     }
-    @LifecycleMeta(S1.class)
-    static class NLM_With_PreStateChange_To_Possible_Next_State {
+    @StateMachine
+    static interface S2 {
 
-        /***
-         * prestatechange
-         * conditional
-         * pre-evaluate
-         * from
-         * ok
-         * to
-         * ok
-         * post-evaluate
-         * from
-         * ok
-         * to
-         * ???
-         */
+        @StateSet
+        static interface States {
+
+            @Initial
+            @Function(transition = Move.class, value = { S2_State_B.class })
+            static interface S2_State_A {}
+            @InboundWhile(on = { S1.States.S1_State_B.class }, relation = S1Relation.class)
+            @End
+            static interface S2_State_B {}
+        }
+        @TransitionSet
+        static interface Transitions {
+
+            static interface Move {}
+        }
+        @RelationSet
+        static interface Relations {
+
+            @RelateTo(S1.class)
+            static interface S1Relation {}
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class S1BaseLM {
+
         private String state;
 
         @Transition
@@ -96,12 +116,122 @@ public abstract class CallbackTestBase extends BaseMetaDataTest {
         private void setState(String state) {
             this.state = state;
         }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_With_PreStateChange_To_Possible_Next_State extends S1BaseLM {
 
+        /***
+         * prestatechange
+         * conditional
+         * pre-evaluate
+         * from
+         * ok
+         * to
+         * ok
+         * post-evaluate
+         * from
+         * ok
+         * to
+         * ???
+         */
         @PreStateChange(to = S1_State_C.class)
         public void interceptStateChange(LifecycleInterceptor<NLM_With_PreStateChange_To_Possible_Next_State> context) {
             System.out.println("The callback method will not be invoked.");
         }
     }
-    
-    
+    @LifecycleMeta(S1.class)
+    static class NLM_prestatechange_from_state_invalid extends S1BaseLM {
+
+        @PreStateChange(from = S2.States.S2_State_A.class)
+        public void interceptStateChange(LifecycleInterceptor<NLM_prestatechange_from_state_invalid> context) {
+            System.out.println("The from state is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_prestatechange_to_state_invalid extends S1BaseLM {
+
+        @PreStateChange(to = S2.States.S2_State_A.class)
+        public void interceptStateChange(LifecycleInterceptor<NLM_prestatechange_to_state_invalid> context) {
+            System.out.println("The to state is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_poststatechange_from_state_invalid extends S1BaseLM {
+
+        @PostStateChange(from = S2.States.S2_State_A.class)
+        public void interceptStateChange(LifecycleInterceptor<NLM_poststatechange_from_state_invalid> context) {
+            System.out.println("The from state is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_poststatechange_to_state_invalid extends S1BaseLM {
+
+        @PostStateChange(to = S2.States.S2_State_A.class)
+        public void interceptStateChange(LifecycleInterceptor<NLM_poststatechange_to_state_invalid> context) {
+            System.out.println("The to state is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_prestatechange_relation_invalid extends S1BaseLM {
+
+        @PreStateChange(to = S1.States.S1_State_A.class, relation = "s1", mappedBy = "s1")
+        public void interceptStateChange(LifecycleInterceptor<NLM_prestatechange_to_state_invalid> context) {
+            System.out.println("The relation is invalid.");
+        }
+    }
+    @LifecycleMeta(S2.class)
+    static class NLM_prestatechange_mappedby_invalid {
+
+        @Relation(S1Relation.class)
+        private S1BaseLM s1;
+        @StateIndicator
+        private String state;
+
+        public NLM_prestatechange_mappedby_invalid() {
+            state = S2.States.S2_State_A.class.getSimpleName();
+        }
+
+        @Transition
+        public void move() {}
+
+        public String getState() {
+            return state;
+        }
+
+        @PreStateChange(to = S2.States.S2_State_A.class, relation = "s1", mappedBy = "s1")
+        public void interceptStateChange(LifecycleInterceptor<NLM_prestatechange_to_state_invalid> context) {
+            System.out.println("The mappedBy is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_poststatechange_relation_invalid extends S1BaseLM {
+
+        @PostStateChange(to = S1.States.S1_State_A.class, relation = "s1", mappedBy = "s1")
+        public void interceptStateChange(LifecycleInterceptor<NLM_prestatechange_to_state_invalid> context) {
+            System.out.println("The relation is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_poststatechange_mappedby_invalid extends S1BaseLM {
+
+        private S1BaseLM s1;
+
+        @PostStateChange(to = S2.States.S2_State_A.class, relation = "s1", mappedBy = "s1")
+        public void interceptStateChange(LifecycleInterceptor<NLM_prestatechange_to_state_invalid> context) {
+            System.out.println("The mappedBy is invalid.");
+        }
+    }
+    @LifecycleMeta(S1.class)
+    static class PLM_With_CallBacksWithDefaultValues extends S1BaseLM {
+
+        @Callbacks
+        public void interceptStates(LifecycleInterceptor<PLM_With_CallBacksWithDefaultValues> context) {}
+    }
+    @LifecycleMeta(S1.class)
+    static class NLM_With_CallBacksWithInvalidStates extends S1BaseLM {
+
+        @Callbacks(preStateChange = { @PreStateChange(from = S2.States.S2_State_A.class) },
+                postStateChange = { @PostStateChange(to = S2.States.S2_State_A.class) })
+        public void interceptStates(LifecycleInterceptor<NLM_With_CallBacksWithInvalidStates> context) {}
+    }
 }
