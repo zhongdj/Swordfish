17d67d0f63461cda005745ccf3c2b850a6f08677
TracyLu <hailinglu@gmail.com>
2013-11-18 21:52:36 +0800

    Added 1 positive test to test @Callbacks with default values

--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/callback/CallbackTestSuite.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/lm/callback/CallbackTestSuite.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-
 @RunWith(Suite.class)
-@SuiteClasses({ CallbackNegativeTests.class })
+@SuiteClasses({ CallbackNegativeTests.class, CallbackPositiveTests.class })
 public class CallbackTestSuite {}
