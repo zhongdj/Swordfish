11fbd9ccad36d095e07a7b9f81d19ce08541b898
Barry Zhong <zhongdj@gmail.com>
2013-11-19 23:50:25 +0800

    organize imports

--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/engine/LifecycleLockTests.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/engine/LifecycleLockTests.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
