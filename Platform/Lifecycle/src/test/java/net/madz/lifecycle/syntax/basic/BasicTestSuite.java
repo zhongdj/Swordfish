1713ff08ed74815f669227bf6562fd0de1fdfa96
TracyLu <hailinglu@gmail.com>
2013-11-26 22:23:51 +0800

    Fixed bug

--- Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/basic/BasicTestSuite.java
+++ Platform/Lifecycle/src/test/java/net/madz/lifecycle/syntax/basic/BasicTestSuite.java
@@ -1,6 +1,6 @@
 package net.madz.lifecycle.syntax.basic;
 
-import net.madz.lifecycle.syntax.lm.transition.TransitionTestSuite;
+import net.madz.lifecycle.syntax.basic.transition.TransitionSyntaxTestSuite;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -8,5 +8,5 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CompositeStateMachineNegativeTests.class, ConditionSetTest.class,
-        StateSetAndTransitionSetSyntaxNegativeTest.class, StateSetSyntaxPositiveTest.class, TransitionTestSuite.class })
+        StateSetAndTransitionSetSyntaxNegativeTest.class, StateSetSyntaxPositiveTest.class, TransitionSyntaxTestSuite.class })
 public class BasicTestSuite {}
